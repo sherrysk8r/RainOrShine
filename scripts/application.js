ac7cd253cb0877f15430874f437c173d11433c56
Sherry <sherryc1@andrew.cmu.edu>
2015-09-30 10:34:38 -0400

    Added key weather information to results page

--- scripts/application.js
+++ scripts/application.js
@@ -15,12 +15,29 @@ $(document).ready(function(){
 	getWeather(sessionStorage.getItem("zipcode"));
 })
 function getWeather(city){
-	url = "http://api.openweathermap.org/data/2.5/forecast?q=" + city+",us&mode=json"
+	// use forecast to get 5 day forecast
+	// problem: it starts from midnight, so will have to create some filtering mechanism
+	url = "http://api.openweathermap.org/data/2.5/weather?q=" + city+",us&units=imperial&mode=json"
 	$.getJSON(url, function(responseObject){
 		var results = responseObject;
 		console.log(results);
+		showWeather(results);
 	});
 }
+
+var weather;
+function showWeather(weatherObj){
+	// will have to be readjusted when we change weather to forecast
+	weather = weatherObj;
+	$('#cityName').text(weatherObj.name);
+	$('#low').text(weatherObj.main.temp_min);
+	$('#high').text(weatherObj.main.temp_max);
+	$('#now').text(weatherObj.main.temp);
+	$('#current_weather').text(weatherObj.weather[0].main);
+	var icon_url = "http://openweathermap.org/img/w/" + weatherObj.weather[0].icon + ".png"
+	$('#weather').prepend("<img src=" + icon_url + ">");
+}
+
 function getExercises(pageNumber){
 	url = "https://wger.de/api/v2/exercise/?page=" + pageNumber + "&format=json"
 	$.getJSON(url, function(responseObject){
