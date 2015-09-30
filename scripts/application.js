443c29f82449247f2289dbc1949ed680438b5a16
Sherry <sherryc1@andrew.cmu.edu>
2015-09-30 12:41:35 -0400

    Randomly selects a workout for each exercise category

--- scripts/application.js
+++ scripts/application.js
@@ -1,26 +1,28 @@
 var englishExercises = [];
-$(document).ready(function(){
+var equipmentList;
+var exerciseCategory;
 
+$(document).ready(function(){
 	var pageNumber = 1;
 	while(pageNumber <= 16){
 		getExercises(pageNumber);
 		pageNumber += 1;
 	}
+	getWeather(sessionStorage.getItem("zipcode"));
+	getEquipmentList();
+	getExerciseCategory();
 
 	$('#submit-button').click(function(){
 		sessionStorage.setItem("zipcode", $("#zipcode").val());
 	});
-	
-	console.log(sessionStorage.getItem("zipcode"));
-	getWeather(sessionStorage.getItem("zipcode"));
 })
+
 function getWeather(city){
 	// use forecast to get 5 day forecast
 	// problem: it starts from midnight, so will have to create some filtering mechanism
 	url = "http://api.openweathermap.org/data/2.5/weather?q=" + city+",us&units=imperial&mode=json"
 	$.getJSON(url, function(responseObject){
 		var results = responseObject;
-		console.log(results);
 		showWeather(results);
 	});
 }
@@ -67,3 +69,65 @@ function getExercises(pageNumber){
 		$.merge(englishExercises, engExercises);
 	});
 }
+
+function getEquipmentList(){
+	url = "https://wger.de/api/v2/equipment/?format=json"
+	$.getJSON(url, function(responseObject){
+		// responseObject also contains a count for the number of objects in the list
+		equipmentList = responseObject.results;
+	});
+}
+
+function getExerciseCategory(){
+	url = "https://wger.de/api/v2/exercisecategory/?format=json"
+	$.getJSON(url, function(responseObject){
+		// responseObject also contains a count for the number of objects in the list
+		exerciseCategory = responseObject.results;
+		addCategoriesToResultPage();
+	});
+
+}
+
+function addCategoriesToResultPage(){
+	var displayText = "";
+	for (var i = 0; i < exerciseCategory.length; i++){
+		var categoryName =  exerciseCategory[i].name;
+		displayText += "<div id=" + categoryName + "><h4>"+categoryName+"</h4><div class='proposed_exercise'"+pickRandomExercise(exerciseCategory[i].id) + "</div></div>"
+	}
+	$('#exercise_proposals').html(displayText);	
+}
+
+function pickRandomExercise(exerciseCategoryID){
+	// console.log(exerciseCategoryID);
+	var exercisesInCategory = englishExercises.map(function(obj){
+		return (obj.category == exerciseCategoryID ? obj : null);
+	});
+	exercisesInCategory = exercisesInCategory.clean(null);
+	
+	if (exercisesInCategory.length < 1){
+		return "No exercises available at this time."
+	}
+	else{
+		var randNum = Math.floor((Math.random() * exercisesInCategory.length));
+		var result = displayExerciseObj(exercisesInCategory[randNum]);
+		return (result);
+	}	
+}
+
+function displayExerciseObj(exercise){
+	var displayText = "";
+	displayText += "<h5>" + exercise.name + "</h5>";
+	displayText += "<p>" + exercise.description + "</p>";
+	return displayText;
+}
+
+// Code Credit: http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
